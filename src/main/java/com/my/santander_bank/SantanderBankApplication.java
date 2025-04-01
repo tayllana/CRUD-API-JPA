c7ec84e4b909f6b1ca3fa69a2aba3aef491887d3
Tayllana Wislla <tayllanawislla@gmail.com>
2025-04-01 15:02:37 -0300

    First commit

---
+++ src/main/java/com/my/santander_bank/SantanderBankApplication.java
@@ -0,0 +1,13 @@
+package com.my.santander_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/santander_bank/SantanderBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.santander_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
